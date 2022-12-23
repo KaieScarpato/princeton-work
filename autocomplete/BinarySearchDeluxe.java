a676fa5f8c80f6587802f5fee19181ff43e165a0
KaieScarpato <116307680+KaieScarpato@users.noreply.github.com>
2022-12-23 14:21:48 -0500

    Update BinarySearchDeluxe.java

--- autocomplete/BinarySearchDeluxe.java
+++ autocomplete/BinarySearchDeluxe.java
@@ -20,17 +20,17 @@ public class BinarySearchDeluxe {
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
             int n = comparator.compare(key, a[mid]);
-            if (n < 0) {
-                hi = mid - 1;
+            if (n == 0 && mid == 0) {
+                return mid;
+            }
+            else if (n == 0 && (comparator.compare(key, a[mid - 1]) > 0)) {
+                return mid;
             }
             else if (n > 0) {
                 lo = mid + 1;
             }
             else {
-                while (mid < a.length && comparator.compare(key, a[mid]) == 0) {
-                    mid--;
-                }
-                return mid + 1;
+                hi = mid - 1;
             }
         }
         return -1;
@@ -48,17 +48,17 @@ public class BinarySearchDeluxe {
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
             int n = comparator.compare(key, a[mid]);
-            if (n < 0) {
+            if (mid == a.length - 1 && n == 0) {
+                return mid;
+            }
+            else if (n == 0 && (comparator.compare(key, a[mid + 1]) < 0)) {
+                return mid;
+            }
+            else if (n < 0) {
                 hi = mid - 1;
             }
-            else if (n > 0) {
-                lo = mid + 1;
-            }
             else {
-                while (mid < a.length && comparator.compare(key, a[mid]) == 0) {
-                    mid++;
-                }
-                return mid - 1;
+                lo = mid + 1;
             }
         }
         return -1;
